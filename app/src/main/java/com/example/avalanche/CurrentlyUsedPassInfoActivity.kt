fa6e76a16823b68303da43afd754b55200d96a8c
StarHand <75840742+Happy377@users.noreply.github.com>
2023-03-07 18:43:36 -0500

    Finished creating the activities

---
+++ app/src/main/java/com/example/avalanche/CurrentlyUsedPassInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.avalanche
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CurrentlyUsedPassInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_currently_used_pass_info)
+    }
+}
---
+++ app/src/main/java/com/example/avalanche/StationInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.avalanche
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StationInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_station_info)
+    }
+}
