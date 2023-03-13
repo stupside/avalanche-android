03be45ada1ea42e746f72792122f43cbbe68eae2
StarHand <75840742+Happy377@users.noreply.github.com>
2023-03-13 19:09:36 -0400

    Commented the errors in the previous commit

--- app/src/main/java/com/example/avalanche/WalletActivity.kt
+++ app/src/main/java/com/example/avalanche/WalletActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
-import com.example.avalanche.layouts.ListLayout
+//import com.example.avalanche.layouts.ListLayout
 import com.example.avalanche.ui.theme.AvalancheTheme
 
 
@@ -16,8 +16,8 @@ class WalletActivity : ComponentActivity() {
             AvalancheTheme {
                 val activityTitle = "Wallet";
                 val floatingButton = true
-                val listLayoutTest = ListLayout(activityTitle, floatingButton)
-                listLayoutTest.ScaffoldBase(modifier = Modifier.fillMaxSize(), activityTitle = activityTitle)
+                //val listLayoutTest = ListLayout(activityTitle, floatingButton)
+                //listLayoutTest.ScaffoldBase(modifier = Modifier.fillMaxSize(), activityTitle = activityTitle)
             }
         }
     }
