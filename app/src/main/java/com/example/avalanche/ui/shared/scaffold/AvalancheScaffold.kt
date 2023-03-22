3a094b99f76405a4dc3f4d91b00ef042e36e0ec1
kilian houpeurt <kilian.houpeurt@gmail.com>
2023-03-22 11:41:39 -0400

    chore: add oidc dependency

--- app/src/main/java/com/example/avalanche/ui/shared/scaffold/AvalancheScaffold.kt
+++ app/src/main/java/com/example/avalanche/ui/shared/scaffold/AvalancheScaffold.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -27,7 +28,7 @@ fun AvalancheScaffold(
                 button.invoke()
             }
         ) { paddingValues ->
-            Box(
+            Surface(
                 modifier = Modifier
                     .padding(paddingValues)
                     .fillMaxSize()
