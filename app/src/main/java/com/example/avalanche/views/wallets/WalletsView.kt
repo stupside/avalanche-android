e0f29176eeeeedaa62c0d8c3a53351e68cef2172
StarHand <dane.reeves01@gmail.com>
2023-04-02 17:46:44 -0400

    Rearranged Popup design in WalletsView + Created Validity system for TicketView (Needs to be moved)

--- app/src/main/java/com/example/avalanche/views/wallets/WalletsView.kt
+++ app/src/main/java/com/example/avalanche/views/wallets/WalletsView.kt
@@ -2,16 +2,23 @@ package com.example.avalanche.views.wallets
 
 import Avalanche.Passport.TicketService
 import android.content.Context
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.CheckCircle
+import androidx.compose.material.icons.sharp.CheckCircle
+import androidx.compose.material.icons.sharp.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.avalanche.R
 import com.example.avalanche.StoresActivity
 import com.example.avalanche.WalletActivity
 import com.example.avalanche.core.ui.shared.AvalancheActionConfiguration
@@ -19,6 +26,8 @@ import com.example.avalanche.core.ui.shared.AvalancheColoredBadge
 import com.example.avalanche.core.ui.shared.AvalancheFloatingActionButton
 import com.example.avalanche.core.ui.shared.AvalancheLogo
 import com.example.avalanche.core.ui.shared.list.AvalancheList
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Composable
 fun WalletsView(context: Context, viewModel: WalletsViewModel) {
@@ -59,25 +68,38 @@ fun WalletsView(context: Context, viewModel: WalletsViewModel) {
                     )
                 })
 
+                //TODO: Get rid of this code with it's declarations once done fixing ticketview
+                TicketValidityItem(from = 1680737967, to = 1680910767, isNow = true)
+
                 if (showDialog) {
                     AlertDialog({ showDialog = false }) {
                         Surface(
-                            modifier = Modifier.wrapContentSize(),
+                            //modifier = Modifier.wrapContentSize(),
                             shape = MaterialTheme.shapes.large
                         ) {
                             Column(
-                                modifier = Modifier.padding(16.dp),
-                                verticalArrangement = Arrangement.spacedBy(16.dp)
+                                modifier = Modifier.padding(16.dp)
                             ) {
-                                Row(horizontalArrangement = Arrangement.SpaceBetween) {
-                                    Text("Actions", style = MaterialTheme.typography.titleLarge)
+                                Row(
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                    verticalAlignment = Alignment.CenterVertically,
+                                    modifier = Modifier.fillMaxWidth()
+                                ) {
+                                    Text(
+                                        "Actions",
+                                        modifier = Modifier.padding(horizontal = 16.dp),
+                                        style = MaterialTheme.typography.titleLarge
+                                    )
                                     IconButton(onClick = {
                                         showDialog = false
                                     }) {
                                         Icon(Icons.Default.Close, contentDescription = null)
                                     }
                                 }
-                                Column(verticalArrangement = Arrangement.spacedBy(32.dp)) {
+                                Column(
+                                    verticalArrangement = Arrangement.spacedBy(32.dp),
+                                    modifier = Modifier.fillMaxWidth()
+                                ) {
                                     WalletDialogListItem_ExploreStore(context) {
                                         showDialog = false
                                     }
@@ -94,6 +116,58 @@ fun WalletsView(context: Context, viewModel: WalletsViewModel) {
         }
     })
 }
+
+//TODO: Get ride of this code once TicketActivity works again
+@Composable
+fun TicketValidityItem(from: Long, to: Long, isNow: Boolean) {
+
+    val duration = getDuration(to - from)
+
+    val days = (to - from) / 86400
+    val fromStr = getDateTime(from)
+    val toStr = getDateTime(to)
+
+    ListItem(
+        headlineContent = {
+            Text("$fromStr - $toStr")
+        },
+        trailingContent = {
+            if (isNow) {
+                Icon(
+                    Icons.Default.Done,
+                    contentDescription = null
+                )
+            }
+            /*if (days < 1) {
+                Text("Duration: $duration")
+            } else {
+                Text("Duration: $duration Days")
+            }*/
+        })
+}
+
+private fun getDateTime(seconds: Long): String {
+
+    val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+    return simpleDateFormat.format(seconds * 1000L)
+}
+
+private fun getDuration(seconds: Long): String {
+    return try {
+        val days = seconds / 86400
+        if (days >= 1) {
+            days.toString()
+        } else {
+            val simpleDateFormat = SimpleDateFormat("HH:mm:ss", Locale.ENGLISH)
+            simpleDateFormat.format(seconds * 1000L)
+        }
+    } catch (_: Exception) {
+        "ValueError"
+    }
+}
+
+//TODO: End of the Code to get rid of above
 
 @Composable
 fun WalletsTopBar() {
@@ -125,14 +199,13 @@ fun WalletDialogListItem_ExploreStore(context: Context, onClick: () -> Unit) {
 
             context.startActivity(stores)
         }),
-        leadingContent = {
-            Icon(Icons.Default.ShoppingCart, contentDescription = null)
-        },
         headlineContent = {
-            Text("Explore application stores")
-        }, supportingContent = {
-            Text("Search a stores, create and extend your tickets")
-        })
+            Text("Search a store, create and extend your tickets")
+        },
+        trailingContent = {
+            Icon(Icons.Default.Search, contentDescription = null)
+        }
+    )
 }
 
 @Composable
