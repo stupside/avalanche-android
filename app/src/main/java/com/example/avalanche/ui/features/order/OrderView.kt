4330d57c6c2936f2368f53c33c8a4402cfb24bb4
StarHand <dane.reeves01@gmail.com>
2023-04-28 20:54:38 -0400

    Redesigned the Order View. Date Selector needs to be modified

--- app/src/main/java/com/example/avalanche/ui/features/order/OrderView.kt
+++ app/src/main/java/com/example/avalanche/ui/features/order/OrderView.kt
@@ -8,6 +8,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -17,6 +20,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import avalanche.merchant.plan.Plan.GetOnePlanRpc
 import avalanche.merchant.store.Store.GetOneStoreRpc
@@ -58,20 +62,63 @@ fun OrderView(viewModel: OrderViewModel, planId: String, goBack: () -> Unit, goW
         Column(modifier = Modifier.padding(paddingValues)) {
 
             Column(
-                modifier = Modifier.padding(horizontal = 32.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
+                modifier = Modifier.padding(horizontal = 32.dp)
             ) {
 
                 store?.let {
-                    Text("store: ${it.name}")
+                    ListItem(
+                        headlineContent = {
+                            Text(
+                                fontWeight = FontWeight.SemiBold,
+                                text = "Store"
+                            )
+                        }, supportingContent = {
+                            Text(
+                                style = MaterialTheme.typography.bodyMedium,
+                                fontWeight = FontWeight.Normal,
+                                text = it.name
+                            )
+                        }
+                    )
                 }
 
                 plan?.let {
-                    Row {
-                        Text("free: ${it.isFree}")
-                        Text("price: ${it.price / 100} $")
-                    }
-                    Text("duration: ${TimeUnit.SECONDS.toDays(it.duration.seconds)} days")
+                    ListItem(
+                        headlineContent = {
+                            Text(
+                                fontWeight = FontWeight.SemiBold,
+                                text = "Price"
+                            )
+                        }, supportingContent = {
+                            if (it.isFree)
+                                Text(
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    fontWeight = FontWeight.Normal,
+                                    text = "Free"
+                                )
+                            else
+                                Text(
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    fontWeight = FontWeight.Normal,
+                                    text = "${it.price / 100} $"
+                                )
+                        }
+                    )
+
+                    ListItem(
+                        headlineContent = {
+                            Text(
+                                fontWeight = FontWeight.SemiBold,
+                                text = "Duration"
+                            )
+                        }, supportingContent = {
+                            Text(
+                                style = MaterialTheme.typography.bodyMedium,
+                                fontWeight = FontWeight.Normal,
+                                text = "${TimeUnit.SECONDS.toDays(it.duration.seconds)} days"
+                            )
+                        }
+                    )
                 }
 
                 DatePicker(
